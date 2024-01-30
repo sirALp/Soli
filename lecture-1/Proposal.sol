14a23ac798674fd62723b6638aad1fe2adad147d
Alperen Tekin <121463341+sirALp@users.noreply.github.com>
2024-01-30 22:39:41 +0300

    added calculateCurrentState with its own logic

--- lecture-1/Proposal.sol
+++ lecture-1/Proposal.sol
@@ -7,16 +7,34 @@ pragma solidity ^0.8.19;
 */
 
 contract Task{
+    
+    /* ************** DATAS ******************* */
+
     address owner;
     address[] private voters;  
     uint256 private counter;
 
+    struct Proposal {
+        string title; // title of the proposal --> HOMEWORK 2##
+        string description; // Description of the proposal
+        uint256 approve; // Number of approve votes
+        uint256 reject; // Number of reject votes
+        uint256 pass; // Number of pass votes
+        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
+        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
+        bool is_active; // This shows if others can vote to our contract
+    }
 
+    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
     
     constructor(){
         owner = msg.sender;
         voters.push(msg.sender);
     }
+
+
+
+    /* ***************** Modifiers ***************** */
 
     modifier onlyOwner{
         require(msg.sender == owner, "You don't have permissons!");
@@ -35,18 +53,10 @@ contract Task{
         _;
     }
 
-    struct Proposal {
-        string title; // title of the proposal --> HOMEWORK 2##
-        string description; // Description of the proposal
-        uint256 approve; // Number of approve votes
-        uint256 reject; // Number of reject votes
-        uint256 pass; // Number of pass votes
-        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
-        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
-        bool is_active; // This shows if others can vote to our contract
-    }
 
-    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+    
+    
+    /* ***************** FUNCTIONS ***************** */
 
     function createProposal(
         string calldata _title,
@@ -72,14 +82,17 @@ contract Task{
     function vote(uint8 choice) external active canVote{
         if (choice == 0 ){
             proposal_history[counter].pass++;
+            proposal_history[counter].current_state = calculateCurrentState();
             voters.push(msg.sender);
         }
         else if (choice == 1){
             proposal_history[counter].reject++;
+            proposal_history[counter].current_state = calculateCurrentState();
             voters.push(msg.sender);
         }
         else if (choice == 2){
             proposal_history[counter].approve++;
+            proposal_history[counter].current_state = calculateCurrentState();
             voters.push(msg.sender);
         }
         
@@ -90,15 +103,32 @@ contract Task{
 
         if (totalVote >= proposal_history[counter].total_vote_to_end){
             proposal_history[counter].is_active = false;
+            delete voters;
         }
         
         
     }
 
+    // ::: LOGIC :::
+    // every approval equals +3 points
+    // every rejection equals -3 points
+    // every pass vote equals -1 points
+    // if total point is higher than or equals to 0  
+    // then proposal is passing
+    // otherwise it's failing 
+    // math : approval * 3 >= rejection * 3 + pass 
+    // 
+    // all in all , a pass-vote meaning a third of a reject-vote  
 
+    function calculateCurrentState() private view returns(bool){
+        uint256 approved = proposal_history[counter].approve;
+        uint256 rejected = proposal_history[counter].reject;
+        uint256 passed = proposal_history[counter].pass;
 
-
-
-
+        if (approved*3 >= rejected*3 + passed)
+            return true; // meaning proposal is passing 
+        else 
+            return false;// meaning proposal is failing
+    }
 
 }
